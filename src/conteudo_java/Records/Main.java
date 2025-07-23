b9e3e07f1002aaa4b29950cc21e80aa16c3d5ebb
karoliny bispo <karol-bobio@hotmail.com>
2025-07-23 16:49:46 -0300

    exemplo de Records

--- src/conteudo_java/Records/Main.java
+++ src/conteudo_java/Records/Main.java
@@ -1,4 +1,12 @@
 package conteudo_java.Records;
 
-public class Main {
+
+    public class Main {
+            public static void main(String[] args) {
+
+                Person person = new Person("briana", 20);
+                System.out.println(person);
+                System.out.println(person.nome()); //mostrando apenas o nome
+
+            }
 }
--- src/conteudo_java/Records/Person.java
+++ src/conteudo_java/Records/Person.java
@@ -1,4 +1,12 @@
 package conteudo_java.Records;
 
-public record Person() {
+    public record Person(String nome, int idade) {
+
+        //para definir atributos no records, precisa colocar o 'static'
+        private static String name;
+
+        //construtor no -Records- pode ser usado na regra de negocio, para validar inf etc
+        public Person{
+            System.out.println("Oi");
+        }
 }
